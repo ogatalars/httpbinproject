b3c07097fc0f979be085add473e455d87bacba60
ogatalars <ogataogara@gmail.com>
2024-05-28 16:33:45 -0300

    feat: http status cli

--- python/app/__init__.py
+++ python/app/__init__.py
@@ -1,9 +1,13 @@
-from flask import Flask 
-from app.routes import main 
+from flask import Flask
+from app.routes import main
+from dotenv import load_dotenv
+import os
 
-def create_app(): 
+def create_app():
+    load_dotenv()  
     app = Flask(__name__)
     app.config.from_object('config.DevelopmentConfig')
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
     app.register_blueprint(main)
 
